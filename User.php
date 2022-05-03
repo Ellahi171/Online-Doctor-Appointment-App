b89271972f9b55fea33b8869e121752b147ebdf8
Alekhya122 <104654752+Alekhya122@users.noreply.github.com>
2022-05-03 15:51:55 -0400

    Create User.php

---
+++ User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    const ADMIN = 'ADMIN';
+    const PATIENT = 'PATIENT';
+    const DOCTOR = 'DOCTOR';
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','role','phone_no','address','date_of_birth',
+        'degree','specialty','gender','status','city','state','zip_code'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function schedule()
+    {
+        return $this->hasMany(Schedule::class);
+    }
+}
